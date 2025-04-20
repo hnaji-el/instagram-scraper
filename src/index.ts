ed3efbed2a0ed681515f5875424208130d95c61c
hnaji-el <hamidnaji981@gmail.com>
2025-04-20 22:30:41 +0100

    setup Node.js + Express.js + TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT ?? "3000";
+const domain = process.env.DOMAIN;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("Response sent");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${domain}:${port}`);
+});
